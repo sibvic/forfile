c5b503b85d617d31e5e95a4d48cbcaeddee4b7de
Victor Tereschenko <sibvic@gmail.com>
2021-08-15 08:33:35 +0600

    feat: Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace forfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Not enought parameters\nforfile pattern [params]");
+                return;
+            }
+
+            string[] files = System.IO.Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), args[0]);
+            string arguments = "";
+            for (int i = 2; i < args.Length; ++i)
+                arguments += string.Format("\"{0}\" ", args[i]);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string nameNoExtension = Path.GetFileNameWithoutExtension(file);
+                System.Diagnostics.ProcessStartInfo start = new System.Diagnostics.ProcessStartInfo();
+                start.FileName = args[1];
+                start.Arguments = arguments.Replace("<FILE>", name).Replace("<FILENAME>", nameNoExtension);
+                Console.WriteLine("Processing {0}", start.Arguments);
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                using (System.Diagnostics.Process process = System.Diagnostics.Process.Start(start))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = reader.ReadToEnd();
+                        Console.Write(result);
+                    }
+                }
+            }
+        }
+    }
+}
